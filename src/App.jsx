499a410df1df3bd076d8c6503f11a08dcc5d124e
JANARTHANAN-M-ux <janarthanan03072004@gmail.com>
2026-03-31 12:19:03 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Name from "./Name";
+function App(){
+ return(
+  <>
+  <h1>Hi</h1>
+  <Name name="jana"/>
+  </>
+ );
+}
+export default App;
---
+++ src/Name.jsx
@@ -0,0 +1,8 @@
+function Name(props){
+    return(
+        <>
+        <h1>My Name is {props.name}</h1>
+        </>
+    );
+}
+export default Name;
