5a6aaf55b6dea0b36db47737f784ead4c188b4ae
JANARTHANAN-M-ux <janarthanan03072004@gmail.com>
2026-04-01 17:13:41 +0530

    third commit

--- src/Name.jsx
+++ src/Name.jsx
@@ -1,8 +1,43 @@
-function Name(props){
-    return(
-        <>
-        <h1>My Name is {props.name} iam 21 years old</h1>
-        </>
-    );
+import { useState } from 'react';
+
+function Name() {
+  const [inputs, setInputs] = useState({});
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setInputs(values => ({...values, [name]: value}))
+  }
+
+  return (
+    <form>
+      <label>First name:
+      <input 
+        type="text" 
+        name="firstname" 
+        value={inputs.firstname} 
+        onChange={handleChange}
+      />
+      </label>
+      <label>Last name:
+        <input 
+          type="text" 
+          name="lastname" 
+          value={inputs.lastname} 
+          onChange={handleChange}
+        />
+      </label>
+      <label>Address:
+        <input 
+          type="text" 
+          name="address" 
+          value={inputs.address} 
+          onChange={handleChange}
+        />
+      </label>
+      <p>Current values: {inputs.firstname} {inputs.lastname} {inputs.address} </p>
+      <input type="submit"/>
+    </form>
+  )
 }
 export default Name;
